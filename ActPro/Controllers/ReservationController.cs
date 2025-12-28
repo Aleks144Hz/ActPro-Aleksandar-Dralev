2561bacb42fdf9c0fd3ebbe613d446926b2a2a54
Aleks Dralev <alekdralev@gmail.com>
2025-12-28 03:02:53 +0200

    Added real date to reservation from the db and made the filter and sort function on search page

--- ActPro/Controllers/ReservationController.cs
+++ ActPro/Controllers/ReservationController.cs
@@ -1,6 +1,43 @@
-﻿namespace ActPro.Controllers
+﻿using ActPro.DAL.Data;
+using ActPro.DAL.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace ActPro.Controllers
 {
-    public class ReservationController
+    public class ReservationController : Controller
     {
+        private readonly ApplicationDbContext _context;
+
+        public ReservationController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        public async Task<IActionResult> Index(int id)
+        {
+            var place = await _context.Places
+                .Include(p => p.PlaceImages)
+                .Include(p => p.City)
+                .Include(p => p.Activity)
+                .FirstOrDefaultAsync(m => m.Id == id);
+
+            if (place == null)
+            {
+                return NotFound();
+            }
+
+            return View("Reservation", place);
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Book(int placeId, DateTime date, string timeSlot)
+        {
+            return RedirectToAction("Confirmation", new { id = placeId });
+        }
+        public IActionResult Confirmation(int id)
+        {
+            ViewBag.PlaceId = id;
+            return View();
+        }
     }
 }
